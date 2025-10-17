d5c766a4f573349895c011d5667d95b8b31c8970
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-10-17 19:28:58 +0000

    json

--- log-gossip.sh
+++ log-gossip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/ubuntu/.local/share/solana/install/active_release/bin/solana gossip -um --output json > /home/ubuntu/gossip-out/$(date +"%Y-%m-%d_%H-%M-%S").txt
+/home/ubuntu/.local/share/solana/install/active_release/bin/solana gossip -um --output json > /home/ubuntu/gossip-out/$(date +"%Y-%m-%d_%H-%M-%S").json
